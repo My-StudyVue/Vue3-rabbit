68061c2efac3fdb74132e6520edb170d132295ca
lyxlwz <1850115720@qq.com>
2022-11-13 21:05:01 +0800

    toRef函数

---
+++ Composition-API/src/04-toRef函数.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1> {{ obj.name }}</h1>
+
+  <h1>{{ name }}</h1>
+
+  <button @click="updateName">修改名字</button>
+</template>
+
+<script lang='ts'>
+import { reactive, toRef } from 'vue';
+
+interface Obj {
+  name: string;
+  age: number;
+}
+
+export default {
+  name: 'App',
+  setup() {
+    // 1.响应式数据对象
+    const obj: Obj = reactive({
+      name: '百里守约',
+      age: -100,
+    })
+
+    // 2.模板中只需要使用name数据
+    // 注意：从响应式数据中数据对象中结构出属性数据，不再是响应式数据
+    // let { name } = obj 不能直接解构，出来的是一个普通数据
+    const name = toRef(obj, 'name')
+
+    const updateName = (): void => {
+      // obj.name += '是狗'
+
+      // toRef转换响应式数据包装成对象，value存放值的位置
+      name.value += '是狗'
+    }
+
+    return { obj, name, updateName }
+  },
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
--- Composition-API/src/App.vue
+++ Composition-API/src/App.vue
@@ -1,45 +1,14 @@
 <template>
-  <table border="1">
-    <tr>
-      <th>姓名</th>
-      <th>年龄</th>
-    </tr>
-    <tr>
-      <td>{{ obj?.name }}</td>
-      <td>{{ obj?.age }}</td>
-    </tr>
-  </table>
-
-  <button style="margin-top:10px" @click="updateName">修改数据</button>
+  哈哈哈
 </template>
 
 <script lang='ts'>
-import { reactive } from 'vue'
-
-interface Obj {
-  name: string;
-  age: number;
-}
 
 export default {
   name: 'App',
   setup() {
-    // 普通数据
-    // const obj: Obj = {
-    //   name: '鲁班七号',
-    //   age: 8,
-    // }
-    const obj: Obj = reactive({
-      name: '鲁班七号',
-      age: 8,
-    })
 
-    // 修改名字
-    const updateName = (): void => {
-      obj.name = '东皇太一'
-    }
-
-    return { obj, updateName }
+    return {}
   },
 }
 
