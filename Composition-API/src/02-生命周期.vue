ac2cba48fe00ebb6750ab718f56b4b1c6e0440d6
lyxlwz <1850115720@qq.com>
2022-11-13 19:58:38 +0800

    生命周期钩子

---
+++ Composition-API/src/02-生命周期.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    根组件
+  </div>
+</template>
+
+<script lang="ts">
+import { onBeforeMount, onMounted } from 'vue';
+
+// 组合API的生命周期钩子有7个，可以多次使用同一个钩子，执行顺序和书写顺序相同。
+export default {
+  name: "App",
+ setup(){
+  // 1.DOM渲染前钩子
+  onBeforeMount(() => {
+    console.log('DOM渲染前钩子',document.querySelector('.container'),this);
+  })
+  // 2.DOM渲染后钩子
+  onMounted(() => {
+    console.log('DOM渲染后钩子1',document.querySelector('.container'),this);
+  })
+
+
+  // 我们可以自定义多个相同的构造函数，去实现不同的逻辑
+  onMounted(() => {
+    console.log('DOM渲染后钩子2',document.querySelector('.container'),this);
+  })
+ }
+};
+</script>
+<style lang='less' scoped>
+</style>
+
--- Composition-API/src/App.vue
+++ Composition-API/src/App.vue
@@ -1,34 +1,17 @@
 <template>
-  <h1>
-    {{ msg }}
-  </h1>
-  <button @click="say">你好</button>
+  <div>
+    跟组件
+  </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 export default {
-  name: "App",
-  /**
-   *  1. 组合api的起点，将来的组合api的代码，基本上在这里
-   *  2. setup函数只会在组件初始化的时候执行一次
-   *  3. 可以理解为：在beforeCreate钩子执行前执行，组件实例创建前
-   *  4. 函数中不能使用this ==> undefined
-   *  5. 组件中需要使用的数据和函数，需要在setup中返回
-   */
-  setup() {
-    console.log(this, "====setup");
-    // 数据
-    const msg = "h1 Vue3";
-    // 函数
-    const say = () => {
-      console.log("function Vue3");
-    };
-    return { msg, say };
+  name:'App',
+  setup(){
+
   },
-  beforeCreate() {
-    console.log(this, "====beforeCreate");
-  },
-};
+}
+
 </script>
 <style lang='less' scoped>
 </style>
