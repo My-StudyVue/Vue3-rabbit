6a74dd3e8534801f03dffd6cda3102aa5bebaf87
lyxlwz <1850115720@qq.com>
2022-11-14 11:51:33 +0800

    06-组合API实例

---
+++ Composition-API/src/06-组合API实例.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div>坐标</div>
+    <div>x:{{ x }}</div>
+    <div>y:{{ y }}</div>
+
+    <hr />
+
+    <div>{{ count }}</div>
+    <button @click="add">累加1</button>
+  </div>
+</template>
+
+<script lang='ts'>
+// 总结： 体会组合API的写法，尝试组织可读性高的代码。
+import { onMounted, onUnmounted, reactive, ref, toRefs } from "vue";
+
+const useMouse = () => {
+  /**
+   * 1.   记录鼠标坐标
+   */
+  // 1.1  申明一个响应式数据，他是一个对象，包含x、y
+  const mouse = reactive({
+    x: 0,
+    y: 0,
+  });
+
+  // 1.3修改响应式数据
+  const move = (e) => {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+  };
+
+  // 1.2 等dom渲染完毕
+  onMounted(() => {
+    document.addEventListener("mousemove", move);
+  });
+
+  //1.4 组件销毁，删除事件
+  onUnmounted(() => {
+    document.removeEventListener("mousemove", move);
+  });
+
+  return mouse;
+};
+
+export default {
+  name: "App",
+  setup() {
+    // /**
+    //  * 1.   记录鼠标坐标
+    //  */
+    // // 1.1  申明一个响应式数据，他是一个对象，包含x、y
+    // const mouse = reactive({
+    //   x: 0,
+    //   y: 0,
+    // });
+
+    // // 1.3修改响应式数据
+    // const move = (e) => {
+    //   mouse.x = e.pageX;
+    //   mouse.y = e.pageY;
+    // };
+
+    // // 1.2 等dom渲染完毕
+    // onMounted(() => {
+    //   document.addEventListener("mousemove", move);
+    // });
+
+    // //1.4 组件销毁，删除事件
+    // onUnmounted(() => {
+    //   document.removeEventListener("mousemove", move);
+    // });
+
+    // 封装成函数写法
+    const mouse = useMouse();
+
+    const { x, y } = toRefs(mouse);
+
+    /**
+     * 2.数字累加
+     */
+    const count = ref(0);
+    const add = () => {
+      count.value++;
+    };
+
+    return { x, y, count, add };
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
--- Composition-API/src/App.vue
+++ Composition-API/src/App.vue
@@ -1,90 +1,14 @@
 <template>
-  <table border="1">
-    <tr>
-      <th>姓名</th>
-      <th>年龄</th>
-      <th>shallow非响应式</th>
-      <th>shallow响应式</th>
-    </tr>
-    <tr>
-      <th>{{ name }}</th>
-      <th>{{ age }}</th>
-      <th>{{ shallowName1 }}</th>
-      <th>{{ shallowName2 }}</th>
-    </tr>
-  </table>
-
-  <button style="margin-top:10px" @click="updateName">修改名字</button>
+  <div></div>
 </template>
 
 <script lang='ts'>
-import { ref, shallowRef, triggerRef } from 'vue'
-
 export default {
-  name: 'App',
+  name: "App",
   setup() {
-    // 1.name数据
-    const name = ref<string>('猜猜我是谁')
-    console.log(name, '===name');
-
-    const updateName = () => {
-      name.value = '猜猜'
-    }
-
-    // 2.age数据
-    const age = ref<number>(18)
-
-    /**
-     * ref常用定义简单数据类型的响应式数据
-     * 其实也可以定义复杂数据类型的响应式数据
-     * 对于数据未之的情况下 ref 是最适用的
-     */
-    // const data = ref(null)
-    // setTimeout(()=>{
-    //   data.value = res.data
-    // },1000)
-
-    /**
-     * shallowRef
-     * 
-     * 创建一个跟踪自身 .value 变化的 ref，但不会使其值也变成响应式的
-     * 
-     * 修改其属性是非响应式的这样是不会改变的
-     */
-    let shallowName1 = shallowRef({
-      name: '猜猜我是谁'
-    })
-    setTimeout(() => {
-      shallowName1.value.name = '不猜'
-      console.log(shallowName1, '==shallowName1');
-
-      /**
-       * triggerRef
-       * 
-       * 强制更新页面DOM
-       */
-      triggerRef(shallowName1)
-    }, 1000)
-
-    let shallowName2 = shallowRef(name)
-    setTimeout(() => {
-      // shallowName2.value = '不猜'
-      // console.log(shallowName2, '==shallowName2');
-    }, 2000)
-
-
-    /**
-     * customRef
-     * 自定义ref 
-     * customRef 是个工厂函数要求我们返回一个对象 并且实现 get 和 set  适合去做防抖之类的
-     */
-
-
-    return { name, age, updateName, shallowName1, shallowName2 }
+    return {};
   },
-}
-
+};
 </script>
 <style lang='less' scoped>
-
 </style>
