8f5505b62771313b87e1d819b2a0b4c90d3fcc68
lyxlwz <1850115720@qq.com>
2022-11-14 13:58:40 +0800

    computed基本使用

--- Composition-API/src/App.vue
+++ Composition-API/src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    <div>今年:{{ age }}岁</div>
+    <div>后年:{{ newAge }}岁</div>
+  </div>
 </template>
 
 <script lang='ts'>
+import { computed, ref } from "vue";
 export default {
   name: "App",
   setup() {
-    return {};
+    // 1.计算属性：当你需要依赖现有的响应式数据，根据一定逻辑得到一个新的数据
+    const age = ref(16);
+    // 得到后年的年龄
+    const newAge = computed(() => {
+      // 该函数的返回值就是计算属性的返回值
+      return age.value + 2;
+    });
+    return { age, newAge };
   },
 };
 </script>
