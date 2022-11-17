179687ecb42076f8692469c99f4559100966da34
lyxlwz <1850115720@qq.com>
2022-11-17 21:09:51 +0800

    axios 请求库

---
+++ Project/src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+
+]
+
+// 创建路由实例
+const router = createRouter({
+  //使用hash方式实现路由
+  history: createWebHashHistory(),
+  // 配置路由规则，写法和之前一样
+  routes
+})
+
+export default router
