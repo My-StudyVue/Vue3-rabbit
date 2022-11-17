6cde5f22f9799d8552d9f86bdd1c3442eaa82a5b
lyxlwz <1850115720@qq.com>
2022-11-17 21:38:19 +0800

    样式处理-less 变量与 mixins

--- Project/src/App.vue
+++ Project/src/App.vue
@@ -13,6 +13,18 @@ onMounted(async() =>{
   <h1>Vue3+TS+VueRouter4+Pinia-新版小兔鲜儿</h1>
 </template>
 
-<style scoped>
+<style lang="less">
+// 必须导入 variables.less
+// @import url("@/assets/styles/variables.less");
+// @import url("@/assets/styles/mixins.less");
+
+@import "./assets/styles/variables.less";
+@import "./assets/styles/mixins.less";
+
+
+h1{
+  color: @warnColor;
+  .hoverShadow()
+}
 
 </style>
