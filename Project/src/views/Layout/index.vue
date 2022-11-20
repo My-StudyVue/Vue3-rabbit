0a45fa0c8328d2a473cb32e53431760a9b47ea21
lyxlwz <1850115720@qq.com>
2022-11-20 14:43:49 +0800

    ✨ 配置路由

--- Project/src/views/Layout/index.vue
+++ Project/src/views/Layout/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>Layout</div>
-  <main class="app-body">
+  <div>Layout55555555555555</div>
+  <!-- <main class="app-body"> -->
     <!-- 预留路由出口，添加添加 :key 解决路由缓存的问题 -->
-    <RouterView :key="$route.fullPath" />
+    <!-- <RouterView :key="$route.fullPath" /> -->
     <!-- fulllPath   /category/1029487?username=zs&age=18 -->
     <!-- path        /category/1029487 -->
-  </main>
+  <!-- </main> -->
 </template>
 
 <script lang='ts'>
---
+++ Project/src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Login</div>
+</template>
+
+<script lang='ts'>
+export default {
+  name:'index',
+  components: {},
+  mixins:[],
+
+  props: {},
+  setup(props,context){
+
+  },
+}
+
+</script>
+<style lang='less' scoped>
+</style>
