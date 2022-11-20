b4d49f05de46ce2abe6dca98247c663976979eb9
lyxlwz <1850115720@qq.com>
2022-11-20 17:38:10 +0800

    Axios 二次封装🚨

--- Project/src/store/modules/home.ts
+++ Project/src/store/modules/home.ts
@@ -1,7 +1,16 @@
 import { defineStore } from 'pinia'
 
-import request from '@/utils/request'
+// import request from '@/utils/request'
+// 二次封装引用
+import { http } from '@/utils/request'
 import type { CategoryList } from '@/types'
+
+// 定义axios返回数据的类型
+// 原来的request 需要
+// interface ApiRes<T = unknown> {
+//   msg: string;
+//   result: T;
+// }
 
 // 定义 Store, 命名建议： useXxxxStore
 // 参数1：Store 的唯一标识
@@ -21,7 +30,13 @@ export const useHomeStore = defineStore('home', {
   // 函数/方法,相当于组件的 methods
   actions: {
     async getAllCategories() {
-      const res = await request.get('/home/category/head')
+      // 能用， res.data 的返回值类型为 any
+      // const res = await request.get("/home/category/head");
+
+      // const res = await request.get<ApiRes<CategoryList>>("/home/category/head");
+
+      // 使用起来简洁很多
+      const res = await http<CategoryList>("GET", "/home/category/head");
       this.categoryList = res.data.result
     }
   },
--- Project/src/utils/request.ts
+++ Project/src/utils/request.ts
@@ -1,8 +1,10 @@
-import axios from 'axios'
+import axios, { type Method } from "axios";
+
+export const baseURL = "http://pcapi-xiaotuxian-front-devtest.itheima.net/";
 
 // 创建axios 实例
 const instance = axios.create({
-  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net/',
+  baseURL: baseURL,
   timeout: 5000,
 })
 
@@ -15,7 +17,8 @@ instance.interceptors.request.use(
   error => {
     // 对请求错误做些什么
     return Promise.reject(error)
-  })
+  }
+);
 
 // 添加响应器
 instance.interceptors.response.use(
@@ -26,6 +29,33 @@ instance.interceptors.response.use(
     // 对响应错误做点什么
     return Promise.reject(error)
   }
-)
+);
 
 export default instance
+
+
+
+
+
+
+// 后端返回的接口数据格式
+interface ApiRes<T = unknown> {
+  msg: string;
+  result: T;
+}
+
+/**
+ * axios 二次封装，整合 TS 类型
+ * @param url  请求地址
+ * @param method  请求类型
+ * @param submitData  对象类型，提交数据
+ */
+
+export const http = <T>(method: Method, url: string, submitData?: object) => {
+  return instance.request<ApiRes<T>>({
+    url,
+    method,
+    // 🔔 自动设置合适的 params/data 键名称，如果 method 为 get 用 params 传请求参数，否则用 data
+    [method.toUpperCase() === "GET" ? "params" : "data"]: submitData,
+  })
+}
