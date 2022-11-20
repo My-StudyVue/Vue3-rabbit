85d9e2530b52fb38c8952b33005ed1615a108385
lyxlwz <1850115720@qq.com>
2022-11-20 20:55:41 +0800

    导航menu数据渲染

--- Project/src/views/Home/components/home-category.vue
+++ Project/src/views/Home/components/home-category.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="home-category">分类组件</div>
+  <div class="home-category">
+    <ul class="menu">
+      <li v-for="i in 9" :key="i">
+        <RouterLink to="/">{{ '居家' }}</RouterLink>
+        <RouterLink to="/">{{ '茶咖酒具' }}</RouterLink>
+        <RouterLink to="/">{{ '水具杯壶' }}</RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang='ts'>
@@ -17,5 +25,120 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.home-category {
+  width: 250px;
+  height: 500px;
+  background: rgba(0, 0, 0, 0.8);
+  position: relative;
+  z-index: 99;
 
+  .menu {
+    li {
+      padding-left: 40px;
+      height: 55px;
+      line-height: 55px;
+
+      &:hover {
+        background: @primaryColor;
+      }
+
+      a {
+        margin-right: 4px;
+        color: #fff;
+
+        &:first-child {
+          font-size: 16px;
+        }
+      }
+
+      .layer {
+        width: 990px;
+        height: 500px;
+        background: rgba(255, 255, 255, 1);
+        position: absolute;
+        left: 250px;
+        top: 0;
+        display: none;
+        padding: 0 15px;
+
+        h4 {
+          font-size: 20px;
+          font-weight: normal;
+          line-height: 80px;
+
+          small {
+            font-size: 16px;
+            color: #666;
+          }
+        }
+
+        ul {
+          display: flex;
+          flex-wrap: wrap;
+
+          li {
+            width: 310px;
+            height: 120px;
+            margin-right: 15px;
+            margin-bottom: 15px;
+            border: 1px solid #eee;
+            border-radius: 4px;
+            background: #fff;
+
+            &:nth-child(3n) {
+              margin-right: 0;
+            }
+
+            a {
+              display: flex;
+              width: 100%;
+              height: 100%;
+              align-items: center;
+              padding: 10px;
+
+              &:hover {
+                background: #e3f9f4;
+              }
+
+              img {
+                width: 95px;
+                height: 95px;
+              }
+
+              .info {
+                padding-left: 10px;
+                line-height: 24px;
+                overflow: hidden;
+
+                .name {
+                  font-size: 16px;
+                  color: #666;
+                }
+
+                .desc {
+                  color: #999;
+                }
+
+                .price {
+                  font-size: 22px;
+                  color: @priceColor;
+
+                  i {
+                    font-size: 16px;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      &:hover {
+        .layer {
+          display: block;
+        }
+      }
+    }
+  }
+}
 </style>
