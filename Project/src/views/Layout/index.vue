c0c835b2350c42b5b17ab4caf03ab7b04626ade8
lyxlwz <1850115720@qq.com>
2022-11-20 15:21:21 +0800

    layout布局

--- Project/src/views/Layout/index.vue
+++ Project/src/views/Layout/index.vue
@@ -1,6 +1,9 @@
 <template>
   <!-- 头部导航 -->
   <AppTopNav/>
+  <!-- 头部布局 -->
+  <AppHeader/>
+
   <!-- <main class="app-body"> -->
     <!-- 预留路由出口，添加添加 :key 解决路由缓存的问题 -->
     <!-- <RouterView :key="$route.fullPath" /> -->
@@ -13,10 +16,12 @@
 import { RouterView } from "vue-router";
 
 import AppTopNav from "./components/app-top-nav.vue";
+import AppHeader from "./components/app-header.vue";
 export default {
   name:'index',
   components: {
     AppTopNav,
+    AppHeader,
   },
   mixins:[],
 
