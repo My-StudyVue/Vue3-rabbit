8760137ab79cb464ec3fcb947ff79a4d0eebaf16
lyxlwz <1850115720@qq.com>
2022-11-21 11:20:13 +0800

    💄 首页主体-面板组件封装

---
+++ Project/src/views/Home/components/home-panel.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 左侧标题部分 -->
+        <h3>
+          {{ title }}
+          <small>{{ subTitle }}</small>
+        </h3>
+
+        <!-- 右侧内容区域 -->
+        <slot name="right"></slot>
+      </div>
+
+      <!-- 主体内容区域 -->
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'home-panel',
+  components: {},
+  mixins: [],
+
+  props: {
+    title: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      default: ''
+    },
+  },
+  setup(props, context) {
+
+    return {}
+  },
+}
+</script>
+
+<style lang='less' scoped>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- Project/src/views/Home/index.vue
+++ Project/src/views/Home/index.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="page-home">
+    <!-- 首屏 -->
     <div class="home-enter">
       <div class="container">
+        <!-- 左侧分类 -->
         <HomeCategory />
+        <!-- banner轮播图 -->
         <HomeBanner />
       </div>
     </div>
+
+    <!-- 楼层面板组件和 home-entry 同级 -->
+    <HomePanel title="大标题" sub-title="副标题">
+      <template #right>
+        <XtxMore />
+      </template>
+      <h2>我是主体内容-默认插槽</h2>
+    </HomePanel>
   </div>
 </template>
 
 <script lang='ts'>
 import HomeCategory from './components/home-category.vue';
 import HomeBanner from './components/home-banner.vue';
+import homePanel from './components/home-panel.vue';
+
 export default {
   name: 'index',
   components: {
     HomeCategory,
     HomeBanner,
+    homePanel,
   },
   mixins: [],
 
